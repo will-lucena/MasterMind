291c6225a6857cfb542a874303738d6f7bbe989a
Sarah Raquel <sarah@imd.ufrn.br>
2017-05-28 17:41:12 -0300

    Happy path e sab path pro metodo de adicionarPino da classe Adivinho

---
+++ src/test/java/dominioTest/AdivinhoTest.java
@@ -0,0 +1,65 @@
+package dominioTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dominio.Jogada;
+import dominio.Retorno;
+import exceptions.CorInvalidaException;
+import exceptions.PosicaoInvalidaException;
+import jogo.Tentativa;
+
+public class AdivinhoTest {
+
+	@Test
+	public void test() {
+	//	fail("Not yet implemented");
+	}
+
+	public void realizarTentativaOk(){
+		Jogada j = new Jogada();
+		Tentativa t = new Tentativa();
+		j.setTentativa(t);
+		assertEquals(t, j.getTentativa());
+	}
+	
+	public void realizarTentativaFail(){
+		Jogada j = new Jogada();
+		Tentativa t = new Tentativa();
+		j.setTentativa(t);
+		assertEquals(t, j.getTentativa());
+	}
+	
+	public void jogarOk(){
+		
+		
+		
+	}
+	
+	public void adicionarNovoPinoATentativaOk() throws PosicaoInvalidaException, CorInvalidaException{
+		Jogada j = new Jogada();
+		Tentativa t = new Tentativa();
+		t.adicionarPino(2, "azul");
+		j.setTentativa(t);
+	}
+	
+	//Ao tentar inserir em uma posição inválida, deve disparar a PosicaoInvalidaException
+	@Test (expected = PosicaoInvalidaException.class)
+	public void adicionarNovoPinoATentativaFailPosicaoInvalida() throws  PosicaoInvalidaException, CorInvalidaException{
+		Jogada j = new Jogada();
+		Tentativa t = new Tentativa();
+		t.adicionarPino(4, "azul");
+		j.setTentativa(t);
+	}
+	
+	@Test (expected = CorInvalidaException.class)
+	public void adicionarNovoPinoATentativaFailCorInvalida() throws CorInvalidaException, PosicaoInvalidaException{
+		Jogada j = new Jogada();
+		Tentativa t = new Tentativa();
+		t.adicionarPino(2, "pink");
+		j.setTentativa(t);
+	}
+	
+
+}
---
+++ src/test/java/dominioTest/JogadaTest.java
@@ -0,0 +1,14 @@
+package dominioTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JogadaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
